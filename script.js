2a99fc0f3adbacec2bf3831adc90f6f2b8e7c574
Westhaven School <loganppreece14@gmail.com>
2026-04-17 18:27:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function goTo(page) {
   window.location.href = page;
 }
+
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const error = document.getElementById("error");
+
+  if (!email || !password) {
+    error.textContent = "Please enter username and password";
+    return;
+  }
+
+  // TEMP fake login system (we replace with Firebase later)
+  let role = "Staff";
+
+  if (username.includes("slt")) {
+    role = "SLT";
+  } else if (username.includes("staff")) {
+    role = "Staff";
+  }
+
+  localStorage.setItem("role", role);
+
+  // Go to dashboard
+  window.location.href = "dashboard.html";
+}
